cf501bea563abcaf212ef6c11e2a6b1330f1ac2d
davis21 <davisking21@gmail.com>
2024-03-07 13:52:33 -0500

    Finally got the data to display

--- metar.js
+++ metar.js
@@ -1,44 +1,40 @@
-const apiKey = "a1b94787f5844437bca31238d5";
-const mapKey = "5d1a786878854d19959555d7e7749595";
-const apiUrl = "https://api.checkwx.com/metar/KLYH/decoded";
-
-var xhr = new XMLHttpRequest();
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4) {
-    success(JSON.parse(this.responseText));
-  }
-});
-
-xhr.open("GET", apiUrl);
-xhr.setRequestHeader("X-API-KEY", apiKey);
-xhr.send();
+fetch("https://api.checkwx.com/metar/KLYH/decoded", {
+  method: "GET",
+  headers: {
+    "X-API-KEY": "a1b94787f5844437bca31238d5",
+  },
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    success(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
 
 function success(response) {
   if (response.results > 0) {
-    // We only requested one METAR
-    // If you request more than one, the data array would need to be in a loop
     var metar = response.data[0];
 
-    document.getElementById("icao").innerText = metar.icao + " Weather";
+    document.getElementById("icao").innerText = "ICAO: " + metar.icao;
     document.getElementById("name").innerText = metar.station.name;
-    document.getElementById("raw").innerText = metar.raw_text;
 
-    if (metar.station) {
-      document.getElementById(
-        "coord_block"
-      ).innerHTML = `${metar.station.geometry.coordinates[0]}, ${metar.station.geometry.coordinates[1]}`;
+    if (metar.barometer) {
+      document.getElementById("hg").innerText = metar.barometer.hg + " inHg";
+      document.getElementById("hpa").innerText = metar.barometer.hpa + " hPa";
+      document.getElementById("kpa").innerText = metar.barometer.kpa + " kPa";
+      document.getElementById("mb").innerText = metar.barometer.mb + " mb";
     }
 
-    if (metar.visibility) {
+    /*if (metar.visibility) {
       document.getElementById("visibility_miles").innerText =
         metar.visibility.miles;
       document.getElementById("visibility_block").classList.remove("d-none");
-    }
-
-    if (metar.wind) {
-      document.getElementById("wind_direction").innerText = metar.wind.degrees;
-      document.getElementById("wind_speed").innerText = metar.wind.speed_kts;
-      document.getElementById("wind_block").classList.remove("d-none");
     }
 
     if (metar.clouds[0]) {
@@ -46,13 +42,23 @@ function success(response) {
       metar.clouds.forEach((cloud) => {
         var el = document.createElement("li");
         el.innerHTML =
-          cloud.text + " at " + cloud.meter + " <small> feet AGL</small>";
+          cloud.text +
+          " at " +
+          cloud.base_feet_agl +
+          " <small> feet AGL</small>";
         document.getElementById("cloud_list").appendChild(el);
       });
     }
+
+    if (metar.conditions[0]) {
+      document.getElementById("cond_block").classList.remove("d-none");
+      metar.conditions.forEach((cond) => {
+        var el = document.createElement("li");
+        el.innerHTML = cond.text;
+        document.getElementById("cond_list").appendChild(el);
+      });
+    }*/
   } else {
     document.getElementById("raw").innerText = "No results returned from API";
   }
 }
-
-var xhr1 = new XMLHttpRequest();
